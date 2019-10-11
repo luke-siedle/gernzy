b04e6c5d94822de91d6630ceceda235dd7a93ce5
Luke Siedle <ljsiedle@gmail.com>
2019-10-11 12:15:48 +0200

    Cart migration fix

--- src/database/migrations/2019_01_01_000000_create_cart_table.php
+++ src/database/migrations/2019_01_01_000000_create_cart_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateCartTable extends Migration
 {
@@ -19,7 +19,7 @@ class CreateCartTable extends Migration
             $table->bigInteger('user_id')->nullable();
             $table->bigInteger('session_id')->nullable();
             $table->smallInteger('item_count')->default(0);
-            $table->json('items')->default('[]');
+            $table->json('items')->nullable();
             $table->timestamps();
         });
     }
