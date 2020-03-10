b11be58c0aa76bb6aa2e9d4f81bb932b62dda1f5
Alexeia <alexeiaw@gmail.com>
2020-03-10 12:52:24 +0200

    Seed an initial login detail for the admin client (#84)
    Allows creating of default admin details for logging into admin clients.

--- scripts/seed-environment.sh
+++ scripts/seed-environment.sh
@@ -3,3 +3,4 @@ PARENT_DIR="$(dirname `pwd`)"
 cd $PARENT_DIR/shop ;
 php artisan migrate ;
 php artisan db:seed --class="Gernzy\Server\Database\Seeds\ProductsSeeder" ;
+php artisan db:seed --class="Gernzy\Server\Database\Seeds\AdminClientSeeder" ;
