618392f986965e3db0360c5fa88b162ae67f12d4
Luke Siedle <ljsiedle@gmail.com>
2020-03-13 14:42:08 +0200

    Adds rollup for creating dist of gernzy

--- server/src/resources/js/gernzy.js
+++ server/src/resources/js/gernzy.js
@@ -4,39 +4,49 @@ import { Checkout } from './checkout';
 import { GraphqlService } from './graphqlService';
 import { SessionService } from './session';
 
-// jQuery ajax spinner
-var $loading = $('#loadingDiv').hide();
-$(document)
-    .ajaxStart(function() {
-        $loading.show();
-    })
-    .ajaxStop(function() {
-        $loading.hide();
-    });
+export default {
+    init: function(userConfig = {}) {
+        let config = {
+            ...{
+                apiUrl: 'http://laravel-gernzy.test/graphql',
+            },
+            ...userConfig,
+        };
+        // jQuery ajax spinner
+        var $loading = $('#loadingDiv').hide();
+        $(document)
+            .ajaxStart(function() {
+                $loading.show();
+            })
+            .ajaxStop(function() {
+                $loading.hide();
+            });
 
-let pathname = window.location.pathname;
-let graphQlService = new GraphqlService();
-let sessionService = new SessionService(graphQlService);
-let productObj = new Products(graphQlService);
-let cart = new Cart(productObj, graphQlService);
-let checkout = new Checkout(graphQlService, cart);
+        let pathname = window.location.pathname;
+        let graphQlService = new GraphqlService(config);
+        let sessionService = new SessionService(graphQlService);
+        let productObj = new Products(graphQlService);
+        let cart = new Cart(productObj, graphQlService);
+        let checkout = new Checkout(graphQlService, cart);
 
-// Session setup
-sessionService.setupUser();
-sessionService.setUpShopConfig();
-sessionService.setUpSessionData();
-// sessionService.setUpGeoLocation();
+        // Session setup
+        sessionService.setupUser();
+        sessionService.setUpShopConfig();
+        sessionService.setUpSessionData();
+        // sessionService.setUpGeoLocation();
 
-if (pathname.includes('shop')) {
-    productObj.getAllProducts();
-}
+        if (pathname.includes('shop')) {
+            productObj.getAllProducts();
+        }
 
-if (pathname.includes('cart')) {
-    cart.viewProductsInCart();
-}
+        if (pathname.includes('cart')) {
+            cart.viewProductsInCart();
+        }
 
-if (pathname.includes('checkout')) {
-    checkout.getBasketTotal();
-    checkout.displayLineItems();
-    checkout.checkout();
-}
+        if (pathname.includes('checkout')) {
+            checkout.getBasketTotal();
+            checkout.displayLineItems();
+            checkout.checkout();
+        }
+    },
+};
